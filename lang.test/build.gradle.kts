04a46612a8495e5ab5eb48f14b63cd3ca3cc82d1
Ivo Wilms <ivoatinternet@gmail.com>
2020-05-13 17:21:31 +0200

    Quick commit while gradle integration works

--- lang.test/build.gradle.kts
+++ lang.test/build.gradle.kts
@@ -13,15 +13,25 @@ val daggerVersion = "2.25.2"
 dependencies {
   api(platform(project(":pie.depconstraints")))
 
-  annotationProcessor("com.google.dagger:dagger-compiler:$daggerVersion")
-
-  compileOnly("org.checkerframework:checker-qual-android")
-
   testImplementation("org.metaborg:resource")
   testImplementation(project(":pie.runtime"))
   testImplementation(project(":pie.lang.runtime.java"))
 
+  testCompileOnly("org.checkerframework:checker-qual-android")
+
+//  api(platform(project(":pie.depconstraints")))
+//
+//  // Main
+//  api(project(":pie.api"))
+
+  api("com.google.dagger:dagger:$daggerVersion")
+  annotationProcessor("com.google.dagger:dagger-compiler:$daggerVersion")
+
+  compileOnly("org.checkerframework:checker-qual-android")
+
+  // Test
+//  testImplementation(project(":pie.runtime"))
+
   testAnnotationProcessor("com.google.dagger:dagger-compiler:$daggerVersion")
 
-  testCompileOnly("org.checkerframework:checker-qual-android")
 }
