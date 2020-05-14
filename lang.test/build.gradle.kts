4dabd23748c999425c1a356e175ebb20a63bc62f
Ivo Wilms <ivoatinternet@gmail.com>
2020-05-14 09:56:53 +0200

    change build file because it failed again

--- lang.test/build.gradle.kts
+++ lang.test/build.gradle.kts
@@ -12,26 +12,18 @@ val daggerVersion = "2.25.2"
 
 dependencies {
   api(platform(project(":pie.depconstraints")))
-
-  testImplementation("org.metaborg:resource")
-  testImplementation(project(":pie.runtime"))
-  testImplementation(project(":pie.lang.runtime.java"))
-
-  testCompileOnly("org.checkerframework:checker-qual-android")
-
-//  api(platform(project(":pie.depconstraints")))
-//
-//  // Main
-//  api(project(":pie.api"))
-
   api("com.google.dagger:dagger:$daggerVersion")
+
   annotationProcessor("com.google.dagger:dagger-compiler:$daggerVersion")
 
   compileOnly("org.checkerframework:checker-qual-android")
 
-  // Test
-//  testImplementation(project(":pie.runtime"))
-
   testAnnotationProcessor("com.google.dagger:dagger-compiler:$daggerVersion")
 
+  testCompileOnly("org.checkerframework:checker-qual-android")
+
+  testImplementation("org.metaborg:resource")
+  testImplementation(project(":pie.runtime"))
+  testImplementation(project(":pie.lang.runtime.java"))
+  testImplementation(project(":pie.dagger"))
 }
